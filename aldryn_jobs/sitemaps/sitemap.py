03f5dc0da75f914bd2a981a3073a071fbce39491
Paulo <commonzenpython@gmail.com>
2013-09-20 19:31:10 -0400

    Adds sitemap

---
+++ aldryn_jobs/sitemaps/sitemap.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from django.contrib.sitemaps import Sitemap
+
+from ..models import JobCategory, JobOffer
+
+
+class JobOfferCategoriesSitemap(Sitemap):
+    changefreq = "monthly"
+    priority = 0.5
+
+    def items(self):
+        return JobCategory.objects.all()
+
+
+class JobOfferSitemap(Sitemap):
+    changefreq = "monthly"
+    priority = 0.5
+
+    def items(self):
+        return JobOffer.active.all()
+
+    def lastmod(self, obj):
+        return obj.publication_start
