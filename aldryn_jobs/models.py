abfcf0f062818f2688110199fbbfde63256229a7
Joseph Melettukunnel <jmelett@gmail.com>
2014-12-02 17:48:04 +0100

    0.5.0 update

--- aldryn_jobs/models.py
+++ aldryn_jobs/models.py
@@ -123,10 +123,10 @@ class ActiveJobOffersManager(TranslationManager):
         return self.apply_custom_filters(qs)
 
     def _make_queryset(self, klass, core_filters):
-        # Added for >=hvad 0.6.0 compatibility
+        # Added for >=hvad 0.5.0 compatibility
         qs = super(ActiveJobOffersManager, self)._make_queryset(klass, core_filters)
         import hvad
-        if hvad.VERSION >= (0, 6, 0):
+        if hvad.VERSION >= (0, 5, 0):
             return self.apply_custom_filters(qs)
         return qs
 
