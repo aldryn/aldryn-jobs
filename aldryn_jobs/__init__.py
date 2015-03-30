59d57a1e3699c8c0f16603475c76c14108279b90
Felipe Prenholato <philipe.rp@gmail.com>
2015-03-30 11:16:29 -0300

    Add configuration link to toolbar. Fix edit link. Solves #23.
    Removed `aldryn_jobs.request_job_offer_identifier` that was not
    used have few time and isn't really required.

--- aldryn_jobs/__init__.py
+++ aldryn_jobs/__init__.py
@@ -1,3 +1,2 @@
 # -*- coding: utf-8 -*-
 __version__ = '0.2.2'
-request_job_offer_identifier = 'current_aldryn_jobs_job_offer'
--- aldryn_jobs/cms_toolbar.py
+++ aldryn_jobs/cms_toolbar.py
@@ -1,27 +1,66 @@
 # -*- coding: utf-8 -*-
-from django.core.urlresolvers import reverse
-from django.utils.translation import ugettext_lazy as _
+from aldryn_jobs.models import JobCategory, JobOffer
+from django.core.urlresolvers import reverse, resolve
+from django.utils.translation import ugettext_lazy as _, \
+    get_language_from_request
 
 from cms.toolbar_base import CMSToolbar
 from cms.toolbar_pool import toolbar_pool
 
-from aldryn_jobs import request_job_offer_identifier
 
+def get_joboffer_from_path(path, language):
+    current_url = resolve(path)
+    category_slug = current_url.kwargs['category_slug']
+    job_slug = current_url.kwargs['job_offer_slug']
+    if current_url.url_name == 'job-offer-detail':
+        job_offer = (
+            JobOffer.objects
+                    .language(language)
+                    .translated('en', slug=job_slug)
+                    .filter(category__translations__slug=category_slug,
+                            category__translations__language_code=language)
+                    .get()
+        )
+        return job_offer
+    return None
 
 @toolbar_pool.register
 class JobsToolbar(CMSToolbar):
 
+
     def populate(self):
-        def can(action, model):
-            perm = 'aldryn_jobs.%(action)s_%(model)s' % {'action': action, 'model': model}
-            return self.request.user.has_perm(perm)
+        def can(actions, model):
+            if isinstance(actions, basestring):
+                actions = [actions]
+            for action in actions:
+                perm = 'aldryn_jobs.%(action)s_%(model)s' % {
+                    'action': action, 'model': model
+                }
+                if not self.request.user.has_perm(perm):
+                    return False
+            return True
 
-        if self.is_current_app and (can('add', 'joboffer') or can('change', 'joboffer')):
+        if self.is_current_app and (can(['add', 'change'], 'joboffer') or
+                                    can(['add', 'change'], 'jobsconfig')):
             menu = self.toolbar.get_or_create_menu('jobs-app', _('Jobs'))
-            if can('add', 'joboffer'):
-                menu.add_modal_item(_('Add Job Offer'), reverse('admin:aldryn_jobs_joboffer_add'))
+            if can(['add', 'change'], 'joboffer'):
+                menu.add_modal_item(
+                    _('Add Job Offer'),
+                    reverse('admin:aldryn_jobs_joboffer_add')
+                )
+                language = get_language_from_request(
+                    self.request, check_path=True
+                )
+                job_offer = get_joboffer_from_path(
+                    self.request.path, language
+                )
+                if job_offer:
+                    url = reverse(
+                        'admin:aldryn_jobs_joboffer_change',
+                        args=(job_offer.pk,)
+                    )
+                    menu.add_modal_item(_('Edit Job Offer'), url, active=True)
 
-            job_offer = getattr(self.request, request_job_offer_identifier, None)
-            if job_offer and can('change', 'joboffer'):
-                url = reverse('admin:aldryn_jobs_joboffer_change', args=(job_offer.pk,))
-                menu.add_modal_item(_('Edit Job Offer'), url, active=True)
+            if can(['add', 'change'], 'jobsconfig'):
+                url = reverse('admin:aldryn_jobs_jobsconfig_changelist')
+                menu.add_sideframe_item(_('Configure Jobs Application'), url)
