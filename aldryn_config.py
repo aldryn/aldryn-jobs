641d7c1aeb36ddf1e327c373feeb891f486ef47e
Felipe Prenholato <philipe.rp@gmail.com>
2015-03-30 22:39:34 -0300

    Bump to 0.2.2.2 (0.3a2) with very small fixes.

--- aldryn_config.py
+++ aldryn_config.py
@@ -3,7 +3,11 @@ from aldryn_client import forms
 
 class Form(forms.BaseForm):
 
-    default_send_to = forms.CharField()
+    default_send_to = forms.CharField(
+        "Default send to",
+        help_text="E-mail in this field will be default e-mail to send"
+                  "staff notifications."
+    )
 
     def to_settings(self, data, settings):
         settings['ALDRYN_JOBS_DEFAULT_SEND_TO'] = data['default_send_to']
