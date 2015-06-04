b4db352855f47a48ffb6bd5006259cc6e0e2da81
Kirill Kniazev <kirill.kniazev@divio.ch>
2015-06-04 18:12:07 +0300

    Update test settings. Add correct tempalte processors, loaders, static files finders, fix error in installed apps.

--- test_settings.py
+++ test_settings.py
@@ -6,8 +6,8 @@ HELPER_SETTINGS = {
         'filer',
         'easy_thumbnails',
         'parler',
-	    'aldryn_common',
-        'django_sortedm2m'
+        'aldryn_common',
+        'sortedm2m',
     ],
     'THUMBNAIL_PROCESSORS': (
         'easy_thumbnails.processors.colorspace',
@@ -16,6 +16,38 @@ HELPER_SETTINGS = {
         'easy_thumbnails.processors.filters',
     ),
     'SOUTH_TESTS_MIGRATE': False,
+    'LANGUAGES': (
+        ('en', 'English'),
+        ('de', 'German'),
+    ),
+    'ALDRYN_BOILERPLATE_NAME': 'legacy',
+    'STATICFILES_FINDERS': [
+        'django.contrib.staticfiles.finders.FileSystemFinder',
+        # important! place right before django.contrib.staticfiles.finders.AppDirectoriesFinder
+        'aldryn_boilerplates.staticfile_finders.AppDirectoriesFinder',
+        'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    ],
+    'TEMPLATE_CONTEXT_PROCESSORS': (
+        'django.contrib.auth.context_processors.auth',
+        'django.contrib.messages.context_processors.messages',
+        'django.core.context_processors.i18n',
+        'django.core.context_processors.debug',
+        'django.core.context_processors.request',
+        'django.core.context_processors.media',
+        'django.core.context_processors.csrf',
+        'django.core.context_processors.tz',
+        'sekizai.context_processors.sekizai',
+        'django.core.context_processors.static',
+        'cms.context_processors.cms_settings',
+        'aldryn_boilerplates.context_processors.boilerplate'
+    ),
+    'TEMPLATE_LOADERS': (
+        'django.template.loaders.filesystem.Loader',
+        # important! place right before django.template.loaders.app_directories.Loader
+        'aldryn_boilerplates.template_loaders.AppDirectoriesLoader',
+        'django.template.loaders.app_directories.Loader',
+        'django.template.loaders.eggs.Loader'
+    )
     # 'EMAIL_BACKEND': 'django.core.mail.backends.locmem.EmailBackend',
 }
 
