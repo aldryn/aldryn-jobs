26fa1eca3d68a6c1675ddd1dd5222d5194957205
Martin Koistinen <mkoistinen@gmail.com>
2015-07-14 12:47:41 -0400

    Line length only

--- aldryn_jobs/forms.py
+++ aldryn_jobs/forms.py
@@ -150,7 +150,8 @@ class JobOfferAdminForm(AutoSlugForm):
 
         # small monkey patch to show better label for categories
         def label_from_instance(category_object):
-            return "{0} / {1}".format(category_object.app_config, category_object)
+            return "{0} / {1}".format(
+                category_object.app_config, category_object)
         self.fields['category'].label_from_instance = label_from_instance
 
 
