5d7cf321997e688a9b731e4d0ee4b349ffb18012
Marketionist <marketionist@gmail.com>
2015-07-27 16:33:48 +0300

    Added page creation test

--- aldryn_jobs/tests/frontend/integration/specs/spec.jobs.crud.js
+++ aldryn_jobs/tests/frontend/integration/specs/spec.jobs.crud.js
@@ -4,7 +4,7 @@
  */
 
 'use strict';
-/* global describe, it, browser */
+/* global describe, it, browser, By, expect */
 
 // #############################################################################
 // INTEGRATION TEST
@@ -32,6 +32,84 @@ describe('Aldryn Jobs tests: ', function () {
 
             // login to the site
             jobsPage.cmsLogin();
+        });
+    });
+
+    it('creates a new test page', function () {
+        // click the example.com link in the top menu
+        jobsPage.userMenus.first().click().then(function () {
+            // wait for top menu dropdown options to appear
+            browser.wait(function () {
+                return browser.isElementPresent(jobsPage.userMenuDropdown);
+            }, jobsPage.mainElementsWaitTime);
+
+            return jobsPage.administrationOptions.first().click();
+        }).then(function () {
+            // wait for modal iframe to appear
+            browser.wait(function () {
+                return browser.isElementPresent(jobsPage.sideMenuIframe);
+            }, jobsPage.iframeWaitTime);
+
+            // switch to sidebar menu iframe
+            browser.switchTo().frame(browser.findElement(
+                By.css('.cms_sideframe-frame iframe')));
+
+            browser.wait(function () {
+                return browser.isElementPresent(jobsPage.pagesLink);
+            }, jobsPage.mainElementsWaitTime);
+
+            jobsPage.pagesLink.click();
+
+            // wait for iframe side menu to reload
+            browser.wait(function () {
+                return browser.isElementPresent(jobsPage.addConfigsButton);
+            }, jobsPage.mainElementsWaitTime);
+
+            // check if the page already exists and return the status
+            return jobsPage.addPageLink.isPresent();
+        }).then(function (present) {
+            if (present === true) {
+                // page is absent - create new page
+                browser.wait(function () {
+                    return browser.isElementPresent(jobsPage.addPageLink);
+                }, jobsPage.mainElementsWaitTime);
+
+                jobsPage.addPageLink.click();
+
+                browser.wait(function () {
+                    return browser.isElementPresent(jobsPage.titleInput);
+                }, jobsPage.mainElementsWaitTime);
+
+                jobsPage.titleInput.sendKeys('Test').then(function () {
+                    jobsPage.saveButton.click();
+
+                    return jobsPage.slugErrorNotification.isPresent();
+                }).then(function (present) {
+                    if (present === false) {
+                        browser.wait(function () {
+                            return browser.isElementPresent(jobsPage.editPageLink);
+                        }, jobsPage.mainElementsWaitTime);
+
+                        // wait till the editPageLink will become clickable
+                        browser.sleep(500);
+
+                        // validate/click edit page link
+                        jobsPage.editPageLink.click();
+
+                        // switch to default page content
+                        browser.switchTo().defaultContent();
+
+                        browser.wait(function () {
+                            return browser.isElementPresent(jobsPage.testLink);
+                        }, jobsPage.mainElementsWaitTime);
+
+                        // validate test link text
+                        jobsPage.testLink.getText().then(function (title) {
+                            expect(title).toEqual('Test');
+                        });
+                    }
+                });
+            }
         });
     });
 
