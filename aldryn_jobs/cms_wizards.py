7c436a9499261b571c7fd320e80c5945549bc22e
Kirill Kniazev <kirill.kniazev@divio.ch>
2015-11-04 15:49:45 +0200

    Code clean up.

--- aldryn_jobs/cms_wizards.py
+++ aldryn_jobs/cms_wizards.py
@@ -27,15 +27,12 @@ class ConfigCheckMixin(object):
         :param kwargs: Ignored here
         :return: True if user has add permission, else False
         """
-        # No one can create an Article, if there is no app_config yet.
+        # No one can create a job object, if there is no app_config yet.
         configs = JobsConfig.objects.all()
-        if not configs:
+        if not configs or not any([namespace_is_apphooked(config.namespace)
+                                   for config in configs]):
             return False
-        if not any([namespace_is_apphooked(config.namespace)
-                    for config in configs]):
-            return False
-
-        # Ensure user has permission to create articles.
+        # Ensure user has permission to create job objects.
         if user.is_superuser or user.has_perm(self.perm_string):
             return True
 
@@ -89,7 +86,7 @@ class CreateJobCategoryForm(BaseFormMixin, TranslatableModelForm):
 
 class CreateJobOpeningForm(BaseFormMixin, TranslatableModelForm):
     """
-    The ModelForm for the FAQ Question wizard. Note that Category has few
+    The ModelForm for the Job Opening wizard. Note that JobOpening has few
     translated fields that we need to access, so, we use TranslatableModelForm
     """
 
